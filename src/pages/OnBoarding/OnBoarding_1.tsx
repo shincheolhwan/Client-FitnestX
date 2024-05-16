a4738221594b03fa1050bfe63b9182c3a10c6a81
shincheolhwan <tlscjfhgks@naver.com>
2024-05-16 15:04:50 +0900

    feat(pages/OnBoarding): finish page layout OnBoarding_1

---
+++ src/pages/OnBoarding/OnBoarding_1.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './OnBoarding_1.css';
+
+function OnBoarding_1() {
+    return (
+        <div className={"OnBoarding_1"}>
+            <div className={"content-wrapper"}>
+                <div>
+                    <div>
+                        FitnestX
+                    </div>
+                    <div>
+                        Everybody Can Train
+                    </div>
+                </div>
+            </div>
+            <div className={"button-wrapper"}>
+                <button>Get Started</button>
+            </div>
+        </div>
+    );
+}
+
+export default OnBoarding_1;
