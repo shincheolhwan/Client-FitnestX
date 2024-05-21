00f7218042c4672a99cebffe42de89fd0e172e3f
shincheolhwan <tlscjfhgks@naver.com>
2024-05-21 09:23:31 +0900

    feat: finish OnBoarding_1 UI

--- src/pages/OnBoarding/OnBoarding_1.tsx
+++ src/pages/OnBoarding/OnBoarding_1.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import './OnBoarding_1.scss';
+import Button from "../../components/Button";
 
-function OnBoarding_1() {
+const OnBoarding_1: React.FC = () => {
     return (
         <div className={"OnBoarding_1"}>
             <div className={"content-wrapper"}>
-                <div>
-                    <div>
-                        FitnestX
+                <div className={"title-wrapper"}>
+                    <div className={"title-text"}>
+                        <div>Fitnest</div>
+                        <div className={"X"}>X</div>
                     </div>
-                    <div>
+                    <div className={"sub-title-text"}>
                         Everybody Can Train
                     </div>
                 </div>
             </div>
             <div className={"button-wrapper"}>
-                <button>
-                    Get Started
-                </button>
+                <Button label={"Get Started"}/>
             </div>
         </div>
     );
