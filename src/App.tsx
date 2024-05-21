9df626a80003930ba0de5add825bf88ec60584ec
shincheolhwan <tlscjfhgks@naver.com>
2024-05-21 09:23:10 +0900

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import OnBoarding_1 from "./pages/OnBoarding/OnBoarding_1";
 
 function App() {
-  return (
-      <OnBoarding_1/>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" Component={OnBoarding_1}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
